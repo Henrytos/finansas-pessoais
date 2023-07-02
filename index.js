94150a584133916f9a5f77b3256329da1fbbf3e2
henry <franzhenry46@gmail.com>
2023-07-02 20:01:50 -0300

    Projeto final

--- index.js
+++ index.js
@@ -1,143 +1,137 @@
-const form = document.querySelector('form')
+const form = document.querySelector("form");
 
-form.addEventListener('submit', async (ev) => {
-    ev.preventDefault();
-    const transactionDate = {
-        transaction: document.querySelector('#transaction').value,
-        value: document.querySelector('#value').value
-    }
-    if (transactionDate.transaction !== '' && transactionDate.value !== '') {
-        postTransaction(transactionDate)
-        form.reset()
-    }
-    else {
-        alert('Prencha o formulario')
-    }
-})
+form.addEventListener("submit", async (ev) => {
+  ev.preventDefault();
+  const transactionDate = {
+    transaction: document.querySelector("#transaction").value,
+    value: document.querySelector("#value").value,
+  };
+  if (transactionDate.transaction !== "" && transactionDate.value !== "") {
+    postTransaction(transactionDate);
+    form.reset();
+  } else {
+    alert("Prencha o formulario");
+  }
+});
 
 async function postTransaction(transactionDate) {
-    const response = await fetch('http://localhost:3000/finance', {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json'
-        },
-        body:
-            JSON.stringify(
-                transactionDate
-            )
-    })
-    const result = await response.json()
-    renderTrasaction(result)
+  const response = await fetch("http://localhost:3000/finance", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(transactionDate),
+  });
+  const result = await response.json();
+  renderTrasaction(result);
 }
 
 function renderTrasaction(result) {
-    const card = document.createElement('div')
-    card.classList = `list-group list-group-${result.id}`
-    card.id = result.id
-    const title = document.createElement('h3')
-    title.innerText = result.transaction
+  const card = document.createElement("div");
+  card.classList = `list-group list-group-${result.id}`;
+  card.id = result.id;
+  const title = document.createElement("h3");
+  title.classList = result.value > 0 ? "positeve-title" : "negative-title";
+  title.innerText = result.transaction;
 
-    const money = document.createElement('p')
-    const valueTrasaction = result.value
-    money.innerText = formatarNumero(valueTrasaction)
-    money.classList = valueTrasaction > 0 ? 'positeve' : 'negative'
-    addMoney(valueTrasaction)
+  const money = document.createElement("p");
+  const valueTrasaction = result.value;
+  money.innerText = `RS$ ${formatarNumero(valueTrasaction)}`;
+  money.classList = valueTrasaction > 0 ? "positeve" : "negative";
+  addMoney(valueTrasaction);
 
-    const buttons = document.createElement('span')
-    const btnDELETE = document.createElement('span')
-    btnDELETE.classList = 'btn btn-delete'
-    btnDELETE.innerHTML = '<i class="fa-solid fa-trash" style="color: #ffffff;"></i>'
-    btnDELETE.id = result.id
-    btnDELETE.addEventListener('click', excluitDELETE)
+  const buttons = document.createElement("span");
+  const btnDELETE = document.createElement("span");
+  btnDELETE.classList = "btn btn-delete";
+  btnDELETE.innerHTML =
+    '<i class="fa-solid fa-trash" style="color: #ffffff;"></i>';
+  btnDELETE.id = result.id;
+  btnDELETE.addEventListener("click", excluitDELETE);
 
-
-    const btnPUT = document.createElement('span')
-    btnPUT.classList = 'btn btn-put'
-    btnPUT.innerHTML = '<i class="fa-solid fa-pencil" style="color: #ffffff;"></i>'
-    btnPUT.id = result.id
-    btnPUT.addEventListener('click', editPUT)
-    money.append(buttons)
-    buttons.append(btnDELETE, btnPUT)
-    card.append(title, money)
-    document.querySelector('section').appendChild(card)
-
+  const btnPUT = document.createElement("span");
+  btnPUT.classList = "btn btn-put";
+  btnPUT.innerHTML =
+    '<i class="fa-solid fa-pencil" style="color: #ffffff;"></i>';
+  btnPUT.id = result.id;
+  btnPUT.addEventListener("click", editPUT);
+  money.append(buttons);
+  buttons.append(btnDELETE, btnPUT);
+  card.append(title, money);
+  document.querySelector("section").appendChild(card);
 }
 
 async function excluitDELETE() {
-    await fetch(`http://localhost:3000/finance/${this.id}`, { method: 'DELETE' })
-    location.reload()
-
+  await fetch(`http://localhost:3000/finance/${this.id}`, { method: "DELETE" });
+  document.querySelector("section").innerHTML = "";
+  const articles = await fetch("http://localhost:3000/finance").then((res) =>
+    res.json()
+  );
+  articles.forEach(renderTrasaction);
 }
 async function editPUT() {
+  let editTransaction = prompt("Qual nome da transação");
+  let editValue = prompt("Qual valor");
 
-    let editTransaction = prompt('Qual nome da transação')
-    let editValue = prompt('Qual valor')
-
-
-
-    await fetch(`http://localhost:3000/finance/${this.id}`, {
-        method: 'PUT',
-        headers: {
-            'content-type': 'application/json'
-        },
-        body: JSON.stringify(
-            {
-                transaction: editTransaction,
-                value: editValue,
-                id: this.id
-            }
-        )
-    })
-    location.reload()
-
+  await fetch(`http://localhost:3000/finance/${this.id}`, {
+    method: "PUT",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({
+      transaction: editTransaction,
+      value: editValue,
+      id: this.id,
+    }),
+  });
+  document.querySelector("section").innerHTML = "";
+  const articles = await fetch("http://localhost:3000/finance").then((res) =>
+    res.json()
+  );
+  articles.forEach(renderTrasaction);
 }
-let moneyResult = 0
+let moneyResult = 0;
 function addMoney(money) {
-    moneyResult += parseFloat(money)
-    const saldo = document.getElementById('saldo')
-    saldo.innerText = formatarNumero(moneyResult)
-    saldo.classList = moneyResult > 0 ? 'positeve' : 'negative'
-    situaction(moneyResult > 0)
+  moneyResult += parseFloat(money);
+  const saldo = document.getElementById("saldo");
+  saldo.innerText = ` RS$ ${formatarNumero(moneyResult)}`;
+  saldo.classList = moneyResult > 0 ? "positeve" : "negative";
+  situaction(moneyResult > 0);
 }
 
 function situaction(stateMoney) {
-    const text = document.getElementById('situation')
-    if (stateMoney) {
-        text.innerText = 'Lucro'
-        text.classList.add('positeve')
-
-    } else {
-        text.innerText = 'Prejuizo'
-        text.classList.add('negative')
-    }
+  const text = document.getElementById("situation");
+  if (stateMoney) {
+    text.innerText = " Lucro";
+    text.classList.add("positeve");
+  } else {
+    text.innerText = " Prejuizo";
+    text.classList.add("negative");
+  }
 }
-
 
 async function fetchArticles() {
-    const articles = await fetch("http://localhost:3000/finance").then(res => res.json())
-    articles.forEach(renderTrasaction)
+  const articles = await fetch("http://localhost:3000/finance").then((res) =>
+    res.json()
+  );
+  articles.forEach(renderTrasaction);
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    fetchArticles()
-})
+document.addEventListener("DOMContentLoaded", () => {
+  fetchArticles();
+});
 
 function formatarNumero(numero) {
+  let numeroString = numero.toString();
 
-    let numeroString = numero.toString();
+  let partes = [];
+  while (numeroString.length > 3) {
+    partes.unshift(numeroString.slice(-3));
+    numeroString = numeroString.slice(0, -3);
+  }
 
+  if (numeroString.length > 0) {
+    partes.unshift(numeroString);
+  }
 
-    let partes = [];
-    while (numeroString.length > 3) {
-        partes.unshift(numeroString.slice(-3));
-        numeroString = numeroString.slice(0, -3);
-    }
-
-
-    if (numeroString.length > 0) {
-        partes.unshift(numeroString);
-    }
-
-
-    return partes.join('.');
+  return partes.join(".");
 }
